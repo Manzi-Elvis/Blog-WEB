491b699e7f12097c36ee53ec6cd3aa96a641670d
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-09-24 11:22:08 +0200

    db...

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ import path from 'path';
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(process.env.MONGO)
   .then(() => {
     console.log('MongoDb is connected');
   })
